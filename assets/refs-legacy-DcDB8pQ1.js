b048e1e23efac7da64345850299a6e3180fb479c
zhangjianyuan <1524046652@qq.com>
2024-10-06 04:21:21 +0800

    加了terser后，提交的，second?

--- assets/refs-legacy-DcDB8pQ1.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-kkfIodvL.js"],(function(e,t){"use strict";var r;return{setters:[e=>{r=e.aB}],execute:function(){e("c",((...e)=>t=>{e.forEach((e=>{r(e)?e(t):e.value=t}))}))}}}));
