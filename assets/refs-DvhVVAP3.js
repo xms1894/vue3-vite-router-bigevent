4c09f972bd6da231eb7444e61d983bb314b3451a
zhangjianyuan <1524046652@qq.com>
2024-10-06 14:37:47 +0800

    tomato second commit?

--- assets/refs-DvhVVAP3.js
+++
@@ -1,1 +0,0 @@
-import{aB as a}from"./index-C6yrd5mH.js";const o=(...o)=>r=>{o.forEach((o=>{a(o)?o(r):o.value=r}))};export{o as c};
---
+++ assets/refs-legacy-DcDB8pQ1.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-kkfIodvL.js"],(function(e,t){"use strict";var r;return{setters:[e=>{r=e.aB}],execute:function(){e("c",((...e)=>t=>{e.forEach((e=>{r(e)?e(t):e.value=t}))}))}}}));
